a593bd23062e8d193e4c77011f2363746c6b2833
Zhexin Han <zhexin@Zhexins-MacBook-Pro.local>
2017-12-16 10:53:51 -0800

    Added initial setup

---
+++ Set1/cc1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Module to convert a hex String to a base64 String
+"""
+
+def hex_to_base64(hex_string):
+    """
+    Convert a hex String into a base64 String.
+
+    Arguments:
+        hex_string {String} -- Hex String to convert.
+    """
+
+    pass
