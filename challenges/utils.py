62efa0f47337ef6a0d9d81629deb2a98c126aa4e
Zhexin Han <mikehan1994@gmail.com>
2017-12-16 18:20:16 -0800

    Refactor

--- challenges/utils.py
+++ challenges/utils.py
@@ -4,6 +4,9 @@
 Utils to be used.
 """
 
+import base64
+import codecs
+
 def hex_string_to_binary_string(hex_string):
     """
     Convert a Hex String to a Binary String.
@@ -13,3 +16,16 @@ def hex_string_to_binary_string(hex_string):
     """
 
     return '{0:0>{1}b}'.format(int(hex_string, 16), len(hex_string) * 4)
+
+def get_base64(hex_string):
+    """
+    Get Base64 String from Hex String.
+
+    Arguments:
+        hex_string {String} -- Hex String to be converted.
+
+    Returns:
+        String -- Base64 String.
+    """
+
+    return base64.b64encode(codecs.decode(hex_string, 'hex')).decode()
--- tests/cc1.py
+++ tests/cc1.py
@@ -4,19 +4,15 @@
 Test for Crypto Challenge 1
 """
 
-import base64
-import codecs
 import random
 from unittest import TestCase
 from challenges.cc1 import hex_to_base64
+from challenges.utils import get_base64
 
 class TestCC1(TestCase):
     """
     Test Class for CC1
     """
-
-    def _get_base64(self, hex_string):
-        return base64.b64encode(codecs.decode(hex_string, 'hex')).decode()
 
     def test_hex_to_base64_short(self):
         """
@@ -24,7 +20,7 @@ class TestCC1(TestCase):
         """
 
         input_hex = '44616e676572207a6f6e6521'
-        self.assertEqual(self._get_base64(input_hex), hex_to_base64(input_hex))
+        self.assertEqual(get_base64(input_hex), hex_to_base64(input_hex))
 
     def test_hex_to_base64_long(self):
         """
@@ -32,7 +28,7 @@ class TestCC1(TestCase):
         """
 
         input_hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-        self.assertEqual(self._get_base64(input_hex), hex_to_base64(input_hex))
+        self.assertEqual(get_base64(input_hex), hex_to_base64(input_hex))
 
     def test_hex_to_base64_random(self):
         """
@@ -41,5 +37,5 @@ class TestCC1(TestCase):
 
         for i in range(2, 1000, 2):
             input_hex = ''.join(random.choices('0123456789ABCDEF', k=i))
-            output_bese64 = self._get_base64(input_hex)
+            output_bese64 = get_base64(input_hex)
             self.assertEqual(output_bese64, hex_to_base64(input_hex))
